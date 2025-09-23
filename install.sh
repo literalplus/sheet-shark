26bad83e0d8fe5ee6c79dcd1bc8d55b3e801e67e
Philipp Nowak <philipp.nowak@cloudflight.io>
2025-09-23 13:16:25 +0200

    Fox path for desktop entry

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ mkdir -v -p $DESKTOP_FILES
 mkdir -v -p $ICON_FILES
 mkdir -v -p $CONFIG_DIR
 
-cp -v install/sheet-shark.desktop $DESKTOP_FILES/sheet-shark.desktop
+cat install/sheet-shark.desktop | sed "s@HOME@$HOME@" > $DESKTOP_FILES/sheet-shark.desktop
 cp -v install/sheet-shark_48.png $ICON_FILES/48x48/apps/sheet-shark.png
 cp -v install/sheet-shark.png $ICON_FILES/512x512/apps/sheet-shark.png
 cp -v --update=none install/config.yaml $CONFIG_DIR/config.yaml
